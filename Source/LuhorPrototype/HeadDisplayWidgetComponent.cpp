e581717045193f80795fc7ba3c5fb512d26988c1
Patrick Nell <patrickqwertz@gmail.com>
2025-07-29 15:25:05 +0200

    Add warning display for enemy attacks

---
+++ Source/LuhorPrototype/HeadDisplayWidgetComponent.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HeadDisplayWidgetComponent.h"
+
+void UHeadDisplayWidgetComponent::Display(FName Display, float Time)
+{
+	UTexture2D** texture{ HeadDisplays.Find(Display) };
+	if (texture == nullptr) return;
+	
+	OnHeadDisplayChanged.Broadcast(*texture, Time);
+}
---
+++ Source/LuhorPrototype/HeadDisplayWidgetComponent.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "HeadDisplayWidgetComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LUHORPROTOTYPE_API UHeadDisplayWidgetComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+
+public:
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHeadDisplayChanged, UTexture2D*, Texture, float, Time);
+	UPROPERTY(BlueprintAssignable) FOnHeadDisplayChanged OnHeadDisplayChanged;
+	
+	UFUNCTION(BlueprintCallable)
+	void Display(FName Display, float Time);
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TMap<FName, UTexture2D*> HeadDisplays{};
+};
