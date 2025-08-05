be22923174e509c1dfe381cf8024efa0d24ae06f
Patrick Nell <patrickqwertz@gmail.com>
2025-08-05 13:11:15 +0200

    Add ranged enemy; ranged attacker component

--- Source/LuhorPrototype/RangedAttackProjectile.h
+++ Source/LuhorPrototype/RangedAttackProjectile.h
@@ -3,11 +3,60 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "HittableComponent.h"
 #include "RangedAttackProjectile.generated.h"
+
+class UBoxComponent;
+class UFactionAssociation;
+class URangedAttack;
+
+USTRUCT(BlueprintType)
+struct FProjectileData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	URangedAttack* RangedAttack{};
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UFactionAssociation* SourceFaction{};
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	AActor* Source{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FRotator Direction{};
+};
 
 UCLASS()
 class LUHORPROTOTYPE_API ARangedAttackProjectile : public AActor
 {
 	GENERATED_BODY()
+
+public:
+	ARangedAttackProjectile();
+
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnProjectileHit, const FHittableHitData&, Data);
+ 	UPROPERTY(BlueprintAssignable) FOnProjectileHit OnProjectileHit;
+	
+	void InitializeProjectile(const FProjectileData& Data);
+
+	virtual void Tick(float DeltaSeconds) override;
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UBoxComponent* Collision;
+
+	FProjectileData ProjectileData;
+
+	UFUNCTION()
+	void OnCollisionBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
 };
